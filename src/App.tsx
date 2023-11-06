53517ab67cd60a100e1c8196db2ce133a4cda5f1
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-06 14:26:59 +0100

    dropdown basis

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "./components/layout/Footer";
 import { Header } from "./components/layout/Header";
 import { ImageBox } from "./components/ImageBox";
+import { Dropdown } from "./components/Dropdown";
 
 export const App = () => {
 	return (
@@ -9,6 +10,7 @@ export const App = () => {
 			<p>Welcome to this site.</p>
 			<ImageBox title="Paris Trip" imageFile="paris.png" body="We had a great time in Paris." stars={3} highlighted={true} />
 			<ImageBox title="China Trip" imageFile="city.png" body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo vitae nisi ratione repellendus dolorum ea officia nam, consequuntur, sunt similique dolores quidem iste voluptates adipisci quam suscipit expedita odio commodi!" stars={16} />
+			<Dropdown/>
 			<Footer />
 		</>
 	);
