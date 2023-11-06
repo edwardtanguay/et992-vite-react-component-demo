6bcaa2d45d5734ba8c4625a2b0c81e9313ddc326
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-06 14:23:12 +0100

    boolean finished

--- src/components/ImageBox.tsx
+++ src/components/ImageBox.tsx
@@ -3,13 +3,27 @@ interface IProps {
 	imageFile: string;
 	body: string;
 	stars: number;
+	highlighted?: boolean;
 }
 
-export const ImageBox = ({title, imageFile, body, stars } : IProps) => {
+export const ImageBox = ({
+	title,
+	imageFile,
+	body,
+	stars,
+	highlighted = false,
+}: IProps) => {
 	stars = stars > 5 ? 5 : stars;
+
+	const highlight = () => {
+		return highlighted ? "bg-yellow-300" : "";
+	};
+
 	return (
-		<div className="bg-orange-300 p-4">
-			<h2 className="mb-2 text-2xl">{title} {'*'.repeat(stars)}</h2>
+		<div className={`bg-orange-300 p-4 ${highlight()}`}>
+			<h2 className="mb-2 text-2xl">
+				{title} {"*".repeat(stars)}
+			</h2>
 			<div className="flex gap-4">
 				<img src={`images/${imageFile}`} />
 				<p>{body}</p>
