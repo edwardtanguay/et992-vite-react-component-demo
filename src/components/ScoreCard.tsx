95df55d3b87c8adf5a512ea7ff8b7664c236ec74
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-06 15:46:11 +0100

    invited button in

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import { IPlayerResult } from "../interfaces";
 
 interface IProps {
 	playerResult: IPlayerResult
 }
 export const ScoreCard = ({playerResult}: IProps) => {
+	const [isInvited, setIsInvited] = useState(false);
+
 	return (
 		<div className="flex flex-col bg-green-400 w-[7rem] p-3 items-center justify-center rounded border border-gray-600 mt-4">
-			<p className="font-bold">{playerResult.name}</p>
+			<p className="font-bold">{playerResult.name}{isInvited && '*'}</p>
 			<p>{playerResult.score}</p>
+			<button onClick={() => setIsInvited(!isInvited)}>invited</button>
 		</div>
 	);
 };
