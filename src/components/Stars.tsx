5496f90f9a70d44d6c85962aa9e17a98da6d3f5e
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-06 16:27:01 +0100

    stars

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,7 +1,15 @@
+import { FaGrinStars } from 'react-icons/fa';
+
 interface IProps {
 	stars: number;
 }
 
 export const Stars = ({ stars }: IProps) => {
-	return <>{"*".repeat(stars)}</>;
+
+	const starItems = [];
+	for (let i = 0; i < stars; i++) {
+		starItems.push(<FaGrinStars />);
+	}
+
+	return <div className='flex gap-1 mt-1 mb-3'>{starItems}</div>
 };
