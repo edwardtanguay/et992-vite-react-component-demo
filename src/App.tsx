a2c48613701c30dd358c683e6ac06a4d62e85db0
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-06 11:54:22 +0100

    imagebox finished

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ export const App = () => {
 		<>
 			<Header />
 			<p>Welcome to this site.</p>
+			<ImageBox title="Paris Trip" imageFile="paris.png" body="We had a great time in Paris."/>
 			<ImageBox title="China Trip" imageFile="city.png" body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo vitae nisi ratione repellendus dolorum ea officia nam, consequuntur, sunt similique dolores quidem iste voluptates adipisci quam suscipit expedita odio commodi!" />
 			<Footer />
 		</>
